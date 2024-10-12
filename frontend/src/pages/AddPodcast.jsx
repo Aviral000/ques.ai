f7769e234cae060ac4ad35ec4aea557cf425468e
Aviral000 <avimalik02@gmail.com>
2024-10-12 09:10:56 +0530

    icon changed from react to mui, because of deployment issues with react-icons

--- frontend/src/pages/AddPodcast.jsx
+++ frontend/src/pages/AddPodcast.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { FaPlus } from "react-icons/fa6";
+import AddIcon from '@mui/icons-material/Add';
 import { GoPencil } from "react-icons/go";
 import { FaPodcast } from "react-icons/fa";
 import { RiVipDiamondLine } from "react-icons/ri";
@@ -124,7 +124,7 @@ export default function AddPodcast() {
               <img src={logo} alt="logo" />
           </div>
           <div className={styles.add}>
-            <FaPlus />
+            <AddIcon />
             <Typography variant='p'>Add your Podcast(s)</Typography>
           </div>
           <div className={styles.add1}>
